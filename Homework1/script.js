3616fc8ef6f30de83fa897d8abba0aac6b49e8e3
BojanBozinovski96 <117418699+BojanBozinovski96@users.noreply.github.com>
2022-12-06 19:55:13 +0100

    Add files via upload

---
+++ Homework1/script.js
@@ -0,0 +1,54 @@
+ // Phone price 119.95 // Number of phones 30
+ // Tax price 5%
+
+// Price of the mobile = $119.95
+// saleTax rate = 5%
+// salePrice=125.94
+// Price percent of the mobile = 100% + 5%;
+
+
+
+// 100% price = $119.95
+//     1% price = (119.95 / 100) = 1.119
+//     100% price = (119.95 / 100) * 105 = 125.94
+//  tax per a phone = 125. 94- 114.23 = 5.72
+
+// So the original price is $119.95
+
+ let maxPhones = 30;
+
+ let originalPrice = 119.95;
+
+ let saleTax = 5;
+
+ let salePrice = originalPrice*(1+(saleTax/100));
+
+ console.log(salePrice);
+
+
+ 
+
+ let finalPrice = salePrice*maxPhones;
+
+ finalPrice = 3778.425
+
+ console.log(finalPrice);
+
+ let maxPhonesPrice = originalPrice*maxPhones;
+
+ maxPhonesPrice = 3598.5;
+
+ console.log(maxPhonesPrice);
+ 
+
+ 
+ 
+
+
+
+
+
+
+ 
+
+ 
