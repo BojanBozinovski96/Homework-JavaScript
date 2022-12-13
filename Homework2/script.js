116662a4fcd5ae2b802b1333b19e69f75b4d1d01
BojanBozinovski96 <117418699+BojanBozinovski96@users.noreply.github.com>
2022-12-13 18:15:29 +0100

    Add files via upload

---
+++ Homework2/script.js
@@ -0,0 +1,41 @@
+// For string
+
+let username = "Bojan";
+function forString(username){
+    }
+forString(username);
+console.log(typeof username);
+
+// For Number
+
+let num = 5;
+function forNumber(num){
+}
+forNumber(num);
+console.log(typeof num);
+
+// For boolean
+
+let oolean = true;
+function forBoolean(oolean){
+}
+forBoolean(oolean);
+console.log(typeof oolean);
+
+// For undefined
+
+let parameter = undefined;
+function forUndefined(parameter){
+}
+forUndefined(parameter);
+console.log(typeof parameter);
+
+// For object
+
+let object = null;
+
+function forObject(object){
+
+}
+forObject(object);
+console.log(typeof object);
